c85d0405173d7b0340398edb6c0f9515637b926c
Chandrasegar-123 <92800991+Chandrasegar-123@users.noreply.github.com>
2024-08-14 23:10:18 +0530

    add redux toolkit

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 
 export default function Signin() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false); 
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
@@ -14,8 +16,7 @@ export default function Signin() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
-      setError(false);
+      dispatch(signInStart());
       const response = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -25,15 +26,14 @@ export default function Signin() {
       });
       const data = await response.json();
       console.log(data);
-      setLoading(false);
       if (data.success === false) {
-        setError(true);
+        dispatch(signInFailure(data));
         return;
       }
+      dispatch(signInSuccess(data));
       navigate("/");
     } catch (error) {
-      setLoading(false);
-      setError(true);
+      dispatch(signInFailure(error));
       console.log(error);
       
     }
@@ -67,7 +67,7 @@ export default function Signin() {
           <span className="text-blue-600">Sign Up</span>
         </Link>
       </div>
-      <p className="text-red-500 mt-5">{error && 'Something went wrong!!'}</p>
+      <p className="text-red-500 mt-5">{error ? error.message || 'Something went wrong!!' : ''}</p>
     </div>
   );
 } 
