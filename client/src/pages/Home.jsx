c294c3a449d921a38cb5935cafd32d1fc6c60a22
Chandrasegar-123 <92800991+Chandrasegar-123@users.noreply.github.com>
2024-08-18 19:53:08 +0530

    complete home and about pages

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-700' >Welcome to my Auth App</h1>
+      <p className='mb-4 text-slate-600'>Cross-Origin Resource Sharing (CORS) Issues: The Referrer Policy: strict-origin-when-cross-origin and the lack of content might indicate that the request is being blocked or limited by CORS policies, especially if you're testing locally. However, this is less likely the case because a 204 response means the request was processed.</p>
+      <p className='mb-4 text-slate-600'>Cross-Origin Resource Sharing (CORS) Issues: The Referrer Policy: strict-origin-when-cross-origin and the lack of content might indicate that the request is being blocked or limited by CORS policies, especially if you're testing locally. However, this is less likely the case because a 204 response means the request was processed.</p>
+      <p className='mb-4 text-slate-600'>Cross-Origin Resource Sharing (CORS) Issues: The Referrer Policy: strict-origin-when-cross-origin and the lack of content might indicate that the request is being blocked or limited by CORS policies, especially if you're testing locally. However, this is less likely the case because a 204 response means the request was processed.</p>
+    </div>
   )
 }
